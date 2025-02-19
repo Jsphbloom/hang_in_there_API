3237d4ce7a12fbaa87231d4234877867a2566c48
Mark Kendall <mkendall.tutor@gmail.com>
2025-02-19 14:53:59 -0600

    Add fetch all posters endpoint

---
+++ app/serializers/poster_serializer.rb
@@ -0,0 +1,22 @@
+class PosterSerializer
+  def self.format_posters(posters)
+    poster_data = posters.map do |poster|
+      {
+        #id, type, attributes, name, description, year, price, vintage, url
+        id: poster.id,
+        type: "poster",
+        attributes: {
+          name: poster.name,
+          description: poster.description,
+          price: poster.price,
+          year: poster.year,
+          vintage: poster.vintage,
+          img_url: poster.img_url
+        }
+      }
+    end
+
+    return { data: poster_data }
+  end
+
+end
