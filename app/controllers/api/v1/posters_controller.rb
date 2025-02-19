7da2ddd7b0cb8e803fe9872df9445c58f9c7b3a8
Joseph Bloom <jsphbloom@gmail.com>
2025-02-19 15:45:36 -0700

    can fetch a single poster. added to routs, controller, serializer and tests. most tests not working

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -2,4 +2,7 @@ class Api::V1::PostersController < ApplicationController
   def index
     render json: PosterSerializer.format_posters(Poster.all)
   end
+  def show
+    render json: PosterSerializer.format_single_poster(Poster.find(params[:id]))
+  end
 end
--- spec/requests/api/v1/posters_request_spec.rb
+++ spec/requests/api/v1/posters_request_spec.rb
@@ -55,4 +55,32 @@ describe "hang_in_there_API", type: :request do
     
   end
 
+  it 'can fetch a single poster' do
+    get "/api/v1/posters?1"
+
+    expect(response).to be_successful
+
+    poster_data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(poster_data.count).to eq(1)
+    # expect(poster_data[:data].count).to eq(1)
+    # # expect(poster_data[:id]).to be_a(Integer)
+    # # expect(poster_data).to have_key(:type)
+    # # expect(poster_data[:type]).to be_a(String)
+    # expect(poster_data[:data]).to have_key(:attributes)
+    # expect(poster_data[:attributes]).to be_a(Hash)
+    # expect(poster_data[:attributes]).to have_key(:name)
+    # expect(poster_data[:attributes][:name]).to be_a(String)
+    # expect(poster_data[:attributes]).to have_key(:description)
+    # expect(poster_data[:attributes][:description]).to be_a(String)
+    # expect(poster_data[:attributes]).to have_key(:price)
+    # expect(poster_data[:attributes][:price]).to be_a(Float)
+    # expect(poster_data[:attributes]).to have_key(:year)
+    # expect(poster_data[:attributes][:year]).to be_a(Integer)
+    # expect(poster_data[:attributes]).to have_key(:vintage)
+    # expect([true, false]).to include(poster_data[:attributes][:vintage])   #Weird way to write this test...maybe refactor later
+    # expect(poster_data[:attributes]).to have_key(:img_url)
+    # expect(poster_data[:attributes][:img_url]).to be_a(String)
+  end
+
 end
