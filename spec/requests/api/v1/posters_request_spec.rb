13c534b5278413dcf62b156523fcfae8c1cd7c4d
Joseph Bloom <jsphbloom@gmail.com>
2025-02-20 22:21:39 -0700

    fixed rspec tests, changed before(:each) to before(:do). requires db:reset after each run, will look at a potential refactor using before(:each) again, potentially by removing redunant tests

--- spec/requests/api/v1/posters_request_spec.rb
+++ spec/requests/api/v1/posters_request_spec.rb
@@ -3,7 +3,8 @@ require "simplecov"
 SimpleCov.start
 
 describe "hang_in_there_API", type: :request do
-  before(:each) do
+  before(:all) do
+    
     Poster.create(name: "REGRET",
     description: "Hard work rarely pays off.",
     price: 89.00,
@@ -105,7 +106,7 @@ describe "hang_in_there_API", type: :request do
   end
 
   it 'can fetch a single poster' do
-    get "/api/v1/posters?1"
+    get "/api/v1/posters/1"
 
     expect(response).to be_successful
 
@@ -113,23 +114,23 @@ describe "hang_in_there_API", type: :request do
 
     expect(poster_data.count).to eq(2)
     expect(poster_data[:data].count).to eq(3)
-    expect(poster_data[:data].first).to have_key(:type)
-    expect(poster_data[:data].first[:id]).to be_a(Integer)
-    expect(poster_data[:data].first[:type]).to be_a(String)
-    expect(poster_data[:data].first).to include(:attributes)
-    expect(poster_data[:data].first[:attributes]).to be_a(Hash)
-    expect(poster_data[:data].first[:attributes]).to have_key(:name)
-    expect(poster_data[:data].first[:attributes][:name]).to be_a(String)
-    expect(poster_data[:data].first[:attributes]).to have_key(:description)
-    expect(poster_data[:data].first[:attributes][:description]).to be_a(String)
-    expect(poster_data[:data].first[:attributes]).to have_key(:price)
-    expect(poster_data[:data].first[:attributes][:price]).to be_a(Float)
-    expect(poster_data[:data].first[:attributes]).to have_key(:year)
-    expect(poster_data[:data].first[:attributes][:year]).to be_a(Integer)
-    expect(poster_data[:data].first[:attributes]).to have_key(:vintage)
-    expect([true, false]).to include(poster_data[:data].first[:attributes][:vintage])   #Weird way to write this test...maybe refactor later
-    expect(poster_data[:data].first[:attributes]).to have_key(:img_url)
-    expect(poster_data[:data].first[:attributes][:img_url]).to be_a(String)
+    expect(poster_data[:data]).to have_key(:type)
+    expect(poster_data[:data][:id]).to be_a(Integer)
+    expect(poster_data[:data][:type]).to be_a(String)
+    expect(poster_data[:data]).to include(:attributes)
+    expect(poster_data[:data][:attributes]).to be_a(Hash)
+    expect(poster_data[:data][:attributes]).to have_key(:name)
+    expect(poster_data[:data][:attributes][:name]).to be_a(String)
+    expect(poster_data[:data][:attributes]).to have_key(:description)
+    expect(poster_data[:data][:attributes][:description]).to be_a(String)
+    expect(poster_data[:data][:attributes]).to have_key(:price)
+    expect(poster_data[:data][:attributes][:price]).to be_a(Float)
+    expect(poster_data[:data][:attributes]).to have_key(:year)
+    expect(poster_data[:data][:attributes][:year]).to be_a(Integer)
+    expect(poster_data[:data][:attributes]).to have_key(:vintage)
+    expect([true, false]).to include(poster_data[:data][:attributes][:vintage])   #Weird way to write this test...maybe refactor later
+    expect(poster_data[:data][:attributes]).to have_key(:img_url)
+    expect(poster_data[:data][:attributes][:img_url]).to be_a(String)
   end
 
   it 'can update a poster' do
